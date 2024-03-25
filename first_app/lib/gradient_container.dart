ef7d4a5ea44ba6e8909858976583eac4221e8bb9
MentalJava <107596556+MentalJava@users.noreply.github.com>
2024-03-25 15:42:49 +0900

    Roll Dice

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.startGradient, this.endGradient, {super.key});
+
+  final Color startGradient;
+  final Color endGradient;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [startGradient, endGradient],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
